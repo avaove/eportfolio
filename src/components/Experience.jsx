3082e9ebf81ecf6d222d1f35af6b11108a8cfbdb
avaove <aavaa.oveisi@gmail.com>
2021-12-22 23:27:59 -0500

    updated experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,32 +1,37 @@
 import { React } from "react";
 import { Button } from "react-bootstrap";
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBIcon } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBCardSubTitle } from 'mdb-react-ui-kit';
 import { FaChartBar, FaGithub, FaRegFilePdf } from "react-icons/fa";
 var data = require('../data.json');
 const exps = data["experiences"];
 
 const ExpCard = (props) => {
   console.log(props.desc, "here");
-  let dashboard, dustrepo, poster;
+  let dashboard, dustrepo, poster,image,caption;
   if (props.id == 1) {
     dashboard = <a href="https://unruffled-keller-0e32e0.netlify.app/"><FaChartBar className="icon" /><p>dashboard</p></a>
   } else if (props.id == 2) {
     dustrepo = <a href="https://github.com/avaove/dust-map"><FaGithub className="icon" /></a>;
     poster = <a href="https://docs.google.com/document/d/1_ITitG7YQdSR8rMf2kXE0ggdWRrlQ7sb4wdpFGiopB0/edit"><FaRegFilePdf className="icon" /></a>
   }
+  if (props.img != "") {
+    image = <MDBCardImage position='bottom' src={process.env.PUBLIC_URL + props.img} overlay='white-slight'
+      hover
+      waves />
+      caption = <MDBCardText class="caption">{props.caption}</MDBCardText>
+  }
   return (
     <MDBCard narrow col='4' className='exp-card'>
       <MDBCardBody>
         <MDBCardTitle>{props.job}</MDBCardTitle>
+        <MDBCardSubTitle className=""><a className="company" href={props.link}>{props.company}</a></MDBCardSubTitle>
         <MDBCardText style={{ "text-align": "left" }}><ul>{props.desc.map((point) => <li>{point}</li>)}</ul></MDBCardText>
         <MDBCardText>
           <p className='date'>{props.date}</p>
         </MDBCardText>
       </MDBCardBody>
-      <MDBCardImage position='bottom' src={props.img} overlay='white-slight'
-        hover
-        waves />
-      <MDBCardText class="caption">{props.caption}</MDBCardText>
+      {image}
+      {caption}
       <div>
         {props.skills.map((skill) => <Button disabled variant="dark">{skill}</Button>)}
       </div>
@@ -56,6 +61,7 @@ const Experience = () => {
               skills={exp.skills}
               date={exp.date}
               caption={exp.caption}
+              link={exp.link}
             />
           ))}
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,34 +1,59 @@
+import { Button } from "react-bootstrap";
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+var data = require('../data.json');
+const projs = data["projs"];
+
 const ProjCard = (props) => {
 
   return (
-    <div className="col-lg-4 col-md-12 col-sm-12" style={{"height": "300px"}}>
-      <div className="col-md-12 project">
+    <MDBCard style={{ maxWidth: '22rem' }}>
+      <MDBCardImage src={process.env.PUBLIC_URL + props.img} position='top' alt='Fissure in Sandstone' />
+      <MDBCardBody>
+        <MDBCardTitle>{props.name}</MDBCardTitle>
+        <MDBCardText>
+          {props.desc}
+        </MDBCardText>
         <div>
-          <div></div>
-          <h3 className="project-title">Mock Unix Shell Application</h3>
-          <div className="project-description">In an Agile team, I developed a Unix shell environment in
-            Java using agile design methodologies and adhered to test-driven-development and OOP
-            principles, and used techniques such as dependency-injection. </div>
-        </div>
-        <div>
-          <p className="skills">Java | OOP | TDD</p>
-        </div>
+        {props.skills.map((skill) => <Button disabled variant="dark">{skill}</Button>)}
       </div>
-    </div>
+        {/* <MDBBtn href='#'>Button</MDBBtn> */}
+      </MDBCardBody>
+    </MDBCard>
+    // <div className="col-lg-4 col-md-12 col-sm-12" style={{"height": "300px"}}>
+    //   <div className="col-md-12 project">
+    //     <div>
+    //       <div></div>
+    //       <h3 className="project-title">Mock Unix Shell Application</h3>
+    //       <div className="project-description">In an Agile team, I developed a Unix shell environment in
+    //         Java using agile design methodologies and adhered to test-driven-development and OOP
+    //         principles, and used techniques such as dependency-injection. </div>
+    //     </div>
+    //     <div>
+    //       <p className="skills">Java | OOP | TDD</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
 const Project = (props) => {
   return (
     <section id="projects">
-        <h2 className="heading">───── Projects ─────</h2>
-        <div class="row">
-          <ProjCard />
-          <ProjCard />
-          <ProjCard />
-        </div>
-      </section>
-    
+      <h2 className="heading">───── Projects ─────</h2>
+      <div class="row">
+        {projs.map((proj) => (
+          <ProjCard
+            id={proj.id}
+            img={proj.img}
+            type={proj.type}
+            name={proj.name}
+            desc={proj.desc}
+            skills={proj.skills}
+          />
+        ))}
+      </div>
+    </section>
+
   )
 }
 
