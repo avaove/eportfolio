2a7b7d126ca54a79af6374f77c16893d7d0d60d1
avaove <aavaa.oveisi@gmail.com>
2021-12-24 20:37:21 -0500

    add projects and minor edits to experiences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ const exps = data["experiences"];
 
 const ExpCard = (props) => {
   console.log(props.desc, "here");
-  let dashboard, dustrepo, poster,image,caption;
+  let dashboard, dustrepo, poster, image, caption;
   if (props.id == 1) {
     dashboard = <a href="https://unruffled-keller-0e32e0.netlify.app/"><FaChartBar className="icon" /><p>dashboard</p></a>
   } else if (props.id == 2) {
@@ -15,16 +15,14 @@ const ExpCard = (props) => {
     poster = <a href="https://docs.google.com/document/d/1_ITitG7YQdSR8rMf2kXE0ggdWRrlQ7sb4wdpFGiopB0/edit"><FaRegFilePdf className="icon" /></a>
   }
   if (props.img != "") {
-    image = <MDBCardImage position='bottom' src={process.env.PUBLIC_URL + props.img} overlay='white-slight'
-      hover
-      waves />
-      caption = <MDBCardText class="caption">{props.caption}</MDBCardText>
+    image = <MDBCardImage className='exp-img' position='bottom' src={process.env.PUBLIC_URL + props.img} overlay='white-slight' hover waves />
+    caption = <MDBCardText class="caption">{props.caption}</MDBCardText>
   }
   return (
     <MDBCard narrow col='4' className='exp-card'>
       <MDBCardBody>
         <MDBCardTitle>{props.job}</MDBCardTitle>
-        <MDBCardSubTitle className=""><a className="company" href={props.link}>{props.company}</a></MDBCardSubTitle>
+        <MDBCardSubTitle><a className="company" href={props.link}>{props.company}</a></MDBCardSubTitle>
         <MDBCardText style={{ "text-align": "left" }}><ul>{props.desc.map((point) => <li>{point}</li>)}</ul></MDBCardText>
         <MDBCardText>
           <p className='date'>{props.date}</p>
@@ -32,9 +30,7 @@ const ExpCard = (props) => {
       </MDBCardBody>
       {image}
       {caption}
-      <div>
-        {props.skills.map((skill) => <Button disabled variant="dark">{skill}</Button>)}
-      </div>
+      <div>{props.skills.map((skill) => <Button disabled variant="dark">{skill}</Button>)}</div>
       <br />
       {dashboard}
       <div>
