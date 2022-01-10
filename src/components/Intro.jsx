05abf5241f35846b24104d51f3e8750005d871dc
Ava Oveisi <aavaa.oveisi@gmail.com>
2022-01-10 16:38:42 -0500

    add auto-dashboard project

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,11 +5,12 @@ const Intro = (props) => {
         <h3 style={{ "color": "#64FFB4" }}>Hi, I am</h3>
         <h1 style={{ "color": "#CCD6F6" }}>Ava Oveisi.</h1>
 
-        <p className="intro">I’m an <span style={{ "color": "#64FFB4" }}>Automation and DevOps
-          Developer</span>, and
-          a <span style={{ "color": "#64FFB4" }}>Research Assistant</span>.
+        <p className="intro">I’m a <span style={{ "color": "#64FFB4" }}>
+          Developer</span> and
+          a <span style={{ "color": "#64FFB4" }}>Researcher</span>.
           Currently, I am
-          studying Computer Science and Physical Sciences at the University of Toronto.</p>
+          studying <span style={{ "color": "#64FFB4" }}>Computer Science</span> (SWE Stream) and 
+          <span style={{ "color": "#64FFB4" }}> Physical Sciences</span> at the University of Toronto.</p>
         <a href="#contact" role="button" className="btn btn-outline-primary btn-lg">Get in Touch</a>
       </div>
     </section>
